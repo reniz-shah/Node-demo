44ffb382b262302604f18aadd276325d366c109a
reniz-shah <reniz2810@gmail.com>
2023-01-06 15:51:19 +0530

    1st changes

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 8080;
+app.listen(port,()=>{
+    console.log("Server running on port " + port);
+})
+
+app.get('/', (req, res) => {
+    res.send("Server is running on Prot " + port);
+  })
+
+app.get('/aashihsh', (req, res) => {
+    res.send("This is Aashish Endpoint....");
+  })
